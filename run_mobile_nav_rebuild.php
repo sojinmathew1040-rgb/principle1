49484aaa2013712a9f9d81bf45ce79a49e3795d0
sojinmathew1040-rgb <sojinmathew1040@gmail.com>
2026-08-02 22:10:28 +0530

    refactor: clean up inline styles and consolidate founder component layouts into a new stylesheet

--- run_mobile_nav_rebuild.php
+++
@@ -1,5 +0,0 @@
-<?php
-exec('python c:\\xampp\\htdocs\\principle1\\build_vercel_export.py 2>&1', $output, $return_var);
-echo "Rebuild Output:<br>" . implode("<br>", $output);
-unlink(__FILE__);
-?>
